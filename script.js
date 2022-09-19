a2970eaf0cd210e1e128526fab9201def09fce38
Chin Ying Kong <chinYingAdora>
2022-09-20 00:38:12 +1000

    Added event listener to each cells and added audio

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-// const cell = document.querySelectorAll(".cell");
-// const winningCondition = []
+const cells = document.querySelectorAll(".cell");
+const winningCondition = []
+const restartButton = document.querySelector("button")
 
-const cell = document.querySelector(".cell")
-cell.addEventListener("click", () => {
-    cell.innerHTML = "<img src='img/circleSign.png'>";
+cells.forEach((cell)=>{
+    cell.addEventListener("click", ()=> {
+        cell.innerHTML = "<img src='img/circleSign.png'>";
+        const audio = new Audio("audio/clickSound.mp3");
+        audio.play();
+    }, {once: true})
 })
+
+
+
 //How to switch player?
+//Winning condition
+//Displaying winning message
+//Restarting the game
+// restartButton.addEventListener("click", () => {
+    
+// })
+
+//Partly Done
 //How to stop once I clicked?
     //element.addEventListener("click", listerner, {once:true})
     //the listener will be removed once "click" happened. (not allowed to click 2nd time.)
